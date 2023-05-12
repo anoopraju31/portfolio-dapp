13f581b863990af0be0d0ac239389642459ece07
anoopraju31 <anoopraju31@gmail.com>
2023-05-12 14:36:11 +0530

    Wrapper Children

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Navbar, PagePointer, Sidebar, Socialbar } from './'
 
-const Wrapper = () => {
+const Wrapper = ({ children }) => {
 	return (
 		<div className='w-screen h-screen bg-[var(--wrapper-bg)] relative overflow-hidden'>
 			<div className='max-w-[1728px] md:w-[90%] m-auto'>
@@ -13,7 +13,9 @@ const Wrapper = () => {
 						<Sidebar />
 					</div>
 					<div className='hidden xl:col-span-1 xl:flex bg-gray-600'></div>
-					<div className='col-span-12 md:col-span-10 xl:col-span-8 bg-white'></div>
+					<div className='col-span-12 md:col-span-10 xl:col-span-8 bg-white'>
+						{children}
+					</div>
 					<div className='hidden xl:col-span-1 xl:flex bg-gray-600'></div>
 					<div className='hidden md:col-span-1 md:flex justify-end items-center'>
 						<div className='absolute top-[102px]'>
