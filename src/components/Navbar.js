348b08d5b2ae0543c2fc70381a8e33b1cc959725
anoopraju31 <anoopraju31@gmail.com>
2023-05-12 11:52:16 +0530

    Navbar Responsive Toggle Button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { VscChromeClose } from 'react-icons/vsc'
 import Button from './Button'
+import { useCurrentWidth } from '../hooks'
 
 const Navbar = () => {
 	const [toggleNavbar, setToggleNavbar] = useState(false)
+	const [iconSize, setIconSize] = useState(26)
+	const currentWidth = useCurrentWidth()
+
+	useEffect(() => {
+		setIconSize(currentWidth > 1920 ? 26 : currentWidth > 768 ? 24 : 22)
+	}, [currentWidth])
+
 	return (
-		<div className='flex items-center justify-between my-6'>
+		<div className='flex items-center justify-between my-6 px-4 md:px-0'>
 			<h2 className='text-[var(--green-color)] z-30 font-[400] text-[24px] md:text-[32px] lg:text-[36px] font-handwriting'>
 				Anoop Raju
 			</h2>
@@ -20,11 +28,11 @@ const Navbar = () => {
 				onClick={() => setToggleNavbar((prev) => !prev)}>
 				{toggleNavbar ? (
 					<div className='-z-[9999]'>
-						<VscChromeClose size={30} />
+						<VscChromeClose size={iconSize} />
 					</div>
 				) : (
 					<div className='-z-[9999]'>
-						<GiHamburgerMenu size={30} />
+						<GiHamburgerMenu size={iconSize} />
 					</div>
 				)}
 			</div>
