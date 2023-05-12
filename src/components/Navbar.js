fef05c70e59f34aad15640105e124edf00663cd7
anoopraju31 <anoopraju31@gmail.com>
2023-05-12 10:33:50 +0530

    Navbar Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const Navbar = () => {
-	return <div>Navbar</div>
+	return (
+		<div className='flex items-center justify-between my-6 px-4'>
+			<h2 className='text-[var(--green-color)] z-30 font-[400] text-[24px] md:text-[32px] lg:text-[36px] font-handwriting'>
+				Anoop Raju
+			</h2>
+		</div>
+	)
 }
 
 export default Navbar
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -5,7 +5,7 @@ const Wrapper = () => {
 	return (
 		<div className='w-screen h-screen bg-[var(--wrapper-bg)] relative overflow-hidden'>
 			<div className='max-w-[1728px] md:w-[90%] m-auto'>
-				<div className='h-[96px] bg-green-400'>
+				<div className=''>
 					<Navbar />
 				</div>
 				<div className='grid grid-cols-12 h-[var(--container-height)]'>
