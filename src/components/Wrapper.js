a74a9ae905dcc779c26f2b6e8cd5b4c3345e58b5
anoopraju31 <anoopraju31@gmail.com>
2023-05-12 10:30:11 +0530

    Navbar Initial

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Wrapper = () => {
 	return (
 		<div className='w-screen h-screen bg-[var(--wrapper-bg)] relative overflow-hidden'>
 			<div className='max-w-[1728px] md:w-[90%] m-auto'>
-				<div className='h-[96px] bg-green-400'></div>
+				<div className='h-[96px] bg-green-400'>
+					<Navbar />
+				</div>
 				<div className='grid grid-cols-12 h-[var(--container-height)]'>
 					<div className='hidden md:col-span-1 md:flex bg-slate-600'></div>
 					<div className='hidden md:col-span-1 md:flex bg-gray-600'></div>
