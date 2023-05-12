8f9710d01ad918ba189c0a8af150eca06742b7a3
anoopraju31 <anoopraju31@gmail.com>
2023-05-12 10:28:47 +0530

    Wrapper Layout

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
 const Wrapper = () => {
-	return <div>Wrapper</div>
+	return (
+		<div className='w-screen h-screen bg-[var(--wrapper-bg)] relative overflow-hidden'>
+			<div className='max-w-[1728px] md:w-[90%] m-auto'>
+				<div className='h-[96px] bg-green-400'></div>
+				<div className='grid grid-cols-12 h-[var(--container-height)]'>
+					<div className='hidden md:col-span-1 md:flex bg-slate-600'></div>
+					<div className='hidden md:col-span-1 md:flex bg-gray-600'></div>
+					<div className='col-span-12 md:col-span-8 bg-white'></div>
+					<div className='hidden md:col-span-1 md:flex bg-gray-600'></div>
+					<div className='hidden md:col-span-1 md:flex bg-slate-600'></div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Wrapper
